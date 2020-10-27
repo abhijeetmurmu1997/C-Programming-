c621df09fe28d27a735e89a5e2a5dd50e88e74e0
abhijeetmurmu1997 <abhijeetmurmu1997@gmail.com>
2020-10-27 10:43:39 +0530

    HCF and LCM of 2 numbers

---
+++ HCF or GCD.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	int a,b;
+	scanf("%d %d",&a,&b);
+	int i,hcf = 0,lcm = 0;
+	for(i=1;i<=a || i<=b;i++)
+	{
+		if((a%i == 0) && (b%i == 0))
+		{
+			hcf =i;
+		}
+		lcm = (a*b) / hcf;
+	}
+	printf("HCF : %d\n",hcf);
+	printf("LCM : %d\n",lcm);
+	return 0;
+}
